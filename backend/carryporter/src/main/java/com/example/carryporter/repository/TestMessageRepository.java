9fe0eb8441e1e646c41a2a5fb4fe5c8db91ae645
김정훈 <kik1232198@gmail.com>
2026-01-22 10:43:54 +0900

    디폴트 프로젝트 변경 작업(삭제)

--- backend/carryporter/src/main/java/com/example/carryporter/repository/TestMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.carryporter.repository;
-
-import com.example.carryporter.entity.TestMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestMessageRepository extends JpaRepository<TestMessage, Long> {
-}
