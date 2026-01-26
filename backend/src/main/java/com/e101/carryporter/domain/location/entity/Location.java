21b3a343f14da3a36e4d76150b6d30edc90aa711
전준완 <joownan1024@naver.com>
2026-01-26 17:17:43 +0900

    feat: location entity 및 저장 조회 기능 구현

--- backend/src/main/java/com/e101/carryporter/domain/location/entity/Location.java
+++ backend/src/main/java/com/e101/carryporter/domain/location/entity/Location.java
@@ -1,5 +1,40 @@
 package com.e101.carryporter.domain.location.entity;
 
 
-public class Location {
+import com.e101.carryporter.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "locations")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Location extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String locationName;
+
+    private String description;
+
+    public static Location createLocation(String locatioName, String description) {
+        return Location.builder()
+                .locationName(locatioName)
+                .description(description)
+                .build();
+    }
+
+    @Builder
+    private Location(String locationName, String description) {
+        this.locationName = locationName;
+        this.description = description;
+    }
+
 }
