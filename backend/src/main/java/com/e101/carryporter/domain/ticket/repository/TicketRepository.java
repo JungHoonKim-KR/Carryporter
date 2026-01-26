77c66b03570b8ab611f2e556ea9c65b2a422b55d
전준완 <joownan1024@naver.com>
2026-01-26 16:37:25 +0900

    feat: ticket domain 및 저장 조회 기능 구현

---
+++ backend/src/main/java/com/e101/carryporter/domain/ticket/repository/TicketRepository.java
@@ -0,0 +1,25 @@
+package com.e101.carryporter.domain.ticket.repository;
+
+import com.e101.carryporter.domain.ticket.entity.Ticket;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class TicketRepository {
+
+    private final EntityManager em;
+
+    public Long save(Ticket ticket) {
+        em.persist(ticket);
+        return ticket.getId();
+    }
+
+    public Optional<Ticket> findById(Long ticketId) {
+        return Optional.ofNullable(em.find(Ticket.class, ticketId));
+    }
+
+}
