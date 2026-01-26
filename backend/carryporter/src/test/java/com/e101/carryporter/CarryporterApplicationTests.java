2e43059c671d020defb7f3f7478fab93800dac71
joonwan <joonwan1024@naver.com>
2026-01-26 14:51:47 +0900

    feat: init project

---
+++ backend/carryporter/src/test/java/com/e101/carryporter/CarryporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e101.carryporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarryporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
