5190d8ad18c6d8fca642cc79d717d1d21793ad3e
김정훈 <kik1232198@gmail.com>
2026-01-27 14:25:42 +0900

    [BE] publish 테스트

--- backend/src/main/java/com/e101/carryporter/global/service/mqtt/MqttPublisherService.java
+++ backend/src/main/java/com/e101/carryporter/global/service/mqtt/MqttPublisherService.java
@@ -2,7 +2,6 @@ package com.e101.carryporter.global.service.mqtt;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.MqttHeaders;
 import org.springframework.messaging.Message;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@Profile("!test")
 @RequiredArgsConstructor
 public class MqttPublisherService {
 
