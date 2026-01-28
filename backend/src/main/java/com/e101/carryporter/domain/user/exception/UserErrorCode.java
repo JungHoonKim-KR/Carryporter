27b262b677765b8d5ed866fce53023fc384f8d3d
joonwan1024 <joownan1024@naver.com>
2026-01-28 20:50:37 +0900

    feat: user 조회 service 계층 메서드 구현

---
+++ backend/src/main/java/com/e101/carryporter/domain/user/exception/UserErrorCode.java
@@ -0,0 +1,17 @@
+package com.e101.carryporter.domain.user.exception;
+
+import com.e101.carryporter.global.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+
+    USER_NOT_FOUND("해당 사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),;
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+}
--- backend/src/main/java/com/e101/carryporter/domain/user/user/UserService.java
+++ backend/src/main/java/com/e101/carryporter/domain/user/user/UserService.java
@@ -1,4 +1,22 @@
 package com.e101.carryporter.domain.user.user;
 
+import com.e101.carryporter.domain.user.entity.User;
+import com.e101.carryporter.domain.user.exception.UserErrorCode;
+import com.e101.carryporter.domain.user.repository.UserRepository;
+import com.e101.carryporter.global.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(UserErrorCode.USER_NOT_FOUND));
+    }
 }
