6d6b9d26f630f504df2499541fe73300ad608a01
강희정 <he5298@naver.com>
2026-01-28 21:22:52 +0900

    이벤트 별 필필드 결정

--- backend/src/main/java/com/e101/carryporter/domain/user/event/UserAuthFailedEvent.java
+++ backend/src/main/java/com/e101/carryporter/domain/user/event/UserAuthFailedEvent.java
@@ -1,4 +1,11 @@
 package com.e101.carryporter.domain.user.event;
 
-public record UserAuthFailedEvent() {
+public record UserAuthFailedEvent(
+        Long missionId,
+        int failCount
+) {
+    //잠시 메모용 주석입니당 개발 하면서 삭제할게욥!!
+    //비밀번호 인증 실패를 하면 듣는건 ? -> 실패횟수 체크 핸들러(FailureCountHandler)
+    // FailureCountHandler : 몇번째 틀렸어? -> failCount 필요
+    // 어떤 미션이야? -> missionId 필요
 }
