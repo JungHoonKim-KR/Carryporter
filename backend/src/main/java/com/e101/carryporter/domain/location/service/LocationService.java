8517e726461e2bfb677ec1ce36ebbdb43f4020e6
joonwan1024 <joownan1024@naver.com>
2026-01-28 21:08:11 +0900

    test: location service 조회 테스트 코드 작성:

---
+++ backend/src/main/java/com/e101/carryporter/domain/location/service/LocationService.java
@@ -0,0 +1,23 @@
+package com.e101.carryporter.domain.location.service;
+
+import com.e101.carryporter.domain.location.entity.Location;
+import com.e101.carryporter.domain.location.exception.LocationErrorCode;
+import com.e101.carryporter.domain.location.repository.LocationRepository;
+import com.e101.carryporter.global.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    public Location findById(Long locationId) {
+        return locationRepository.findById(locationId)
+                .orElseThrow(() -> new BusinessException(LocationErrorCode.LOCATION_NOT_FOUND));
+    }
+}
