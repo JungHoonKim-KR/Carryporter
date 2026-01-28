ab34b69d742cf5d1946f813d80d6d8bf0b9956c1
강희정 <he5298@naver.com>
2026-01-28 20:57:26 +0900

    뼈대완성

---
+++ backend/src/main/java/com/e101/carryporter/domain/mission/listener/FailureCountHandler.java
@@ -0,0 +1,9 @@
+package com.e101.carryporter.domain.mission.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class FailureCountHandler {
+}
---
+++ backend/src/main/java/com/e101/carryporter/domain/user/event/UserAuthFailedEvent.java
@@ -0,0 +1,4 @@
+package com.e101.carryporter.domain.user.event;
+
+public record UserAuthFailedEvent() {
+}
