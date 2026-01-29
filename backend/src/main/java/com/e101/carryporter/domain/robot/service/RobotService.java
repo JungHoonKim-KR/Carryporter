8face944b4d0747ea4bf4c5fac0a19b0c316d4be
전준완 <joownan1024@naver.com>
2026-01-29 10:20:24 +0900

    feat: 관리자 lock 관련 서비스 구현

--- backend/src/main/java/com/e101/carryporter/domain/robot/service/RobotService.java
+++ backend/src/main/java/com/e101/carryporter/domain/robot/service/RobotService.java
@@ -1,10 +1,13 @@
 package com.e101.carryporter.domain.robot.service;
 
+import com.e101.carryporter.domain.admin.event.AdminLockRequestEvent;
+import com.e101.carryporter.domain.admin.event.AdminUnlockRequestEvent;
 import com.e101.carryporter.domain.robot.entity.Robot;
 import com.e101.carryporter.domain.robot.exception.RobotErrorCode;
 import com.e101.carryporter.domain.robot.repository.RobotRepository;
 import com.e101.carryporter.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +17,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class RobotService {
 
     private final RobotRepository robotRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     public Robot findById(Long robotId) {
         return robotRepository.findById(robotId)
                 .orElseThrow(() -> new BusinessException(RobotErrorCode.ROBOT_NOT_FOUND));
+    }
+
+    public void lockByAdmin(Long missionId, Long robotId) {
+        Robot robot = findById(robotId);
+        eventPublisher.publishEvent(new AdminLockRequestEvent(missionId, robot.getMacAddress()));
+    }
+
+    public void unlockByAdmin(Long missionId, Long robotId) {
+        Robot robot = findById(robotId);
+        eventPublisher.publishEvent(new AdminUnlockRequestEvent(missionId, robot.getMacAddress()));
     }
 }
--- backend/src/test/java/com/e101/carryporter/domain/robot/service/RobotServiceTest.java
+++ backend/src/test/java/com/e101/carryporter/domain/robot/service/RobotServiceTest.java
@@ -1,13 +1,22 @@
 package com.e101.carryporter.domain.robot.service;
 
+import com.e101.carryporter.domain.admin.event.AdminLockRequestEvent;
+import com.e101.carryporter.domain.admin.event.AdminUnlockRequestEvent;
+import com.e101.carryporter.domain.location.entity.Location;
+import com.e101.carryporter.domain.location.repository.LocationRepository;
+import com.e101.carryporter.domain.mission.entity.Mission;
+import com.e101.carryporter.domain.mission.repository.MissionRepository;
 import com.e101.carryporter.domain.robot.entity.Robot;
 import com.e101.carryporter.domain.robot.repository.RobotRepository;
+import com.e101.carryporter.domain.user.entity.User;
+import com.e101.carryporter.domain.user.repository.UserRepository;
 import com.e101.carryporter.global.exception.BusinessException;
 import com.e101.carryporter.support.IntegrationTestSupport;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.event.ApplicationEvents;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -21,7 +30,19 @@ class RobotServiceTest extends IntegrationTestSupport {
     RobotService robotService;
 
     @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    MissionRepository missionRepository;
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @Autowired
     EntityManager em;
+
+    @Autowired
+    ApplicationEvents events;
 
     @DisplayName("로봇을 pk 기반으로 조회할 수 있다.")
     @Test
@@ -40,6 +61,7 @@ class RobotServiceTest extends IntegrationTestSupport {
         assertThat(findRobot.getMacAddress()).isEqualTo(robot.getMacAddress());
 
     }
+
     @DisplayName("로봇이 없을 경우 예외가 발생한다.")
     @Test
     void findByNotExistsRobotId() {
@@ -50,6 +72,75 @@ class RobotServiceTest extends IntegrationTestSupport {
         assertThatThrownBy(() -> robotService.findById(notExistsRobotId))
                 .isInstanceOf(BusinessException.class)
                 .hasMessage("해당 로봇을 찾을 수 없습니다.");
+
+    }
+
+    @DisplayName("관리자 잠금 해제 요청이 들어올 경우 잠금해제 할 수 있다.")
+    @Test
+    void unlockByAdmin() {
+        // given
+        User user = User.createUser("test@mm.com");
+        userRepository.save(user);
+
+        Robot robot = Robot.createRobot("test code", "aa:bb:cc");
+        robotRepository.save(robot);
+
+        Location callLocation = Location.createLocation("test location", "description", 1.0, 2.0);
+        locationRepository.save(callLocation);
+
+        Mission mission = Mission.createMission(user, callLocation);
+        missionRepository.save(mission);
+
+        flushAndClear();
+
+        // when
+        robotService.unlockByAdmin(mission.getId(), robot.getId());
+
+        // then
+        long publishedCount = events.stream(AdminUnlockRequestEvent.class).count();
+        assertThat(publishedCount).isEqualTo(1);
+
+        AdminUnlockRequestEvent publishedEvent = events.stream(AdminUnlockRequestEvent.class)
+                .findFirst()
+                .orElseThrow();
+
+        assertThat(publishedEvent.missionId()).isEqualTo(mission.getId());
+        assertThat(publishedEvent.robotMacAddress()).isEqualTo(robot.getMacAddress());
+
+    }
+
+    @DisplayName("관리자 잠금 요청이 들어올 경우 잠금 할 수 있다.")
+    @Test
+    void lockByAdmin() {
+        // given
+        User user = User.createUser("test@mm.com");
+        userRepository.save(user);
+
+        Robot robot = Robot.createRobot("test code", "aa:bb:cc");
+        robotRepository.save(robot);
+
+        Location callLocation = Location.createLocation("test location", "description", 1.0, 2.0);
+        locationRepository.save(callLocation);
+
+        Mission mission = Mission.createMission(user, callLocation);
+        missionRepository.save(mission);
+
+        flushAndClear();
+
+        // when
+        System.out.println("robot id " + robot.getId());
+        robotService.lockByAdmin(mission.getId(), robot.getId());
+
+        // then
+        long publishedCount = events.stream(AdminLockRequestEvent.class).count();
+        assertThat(publishedCount).isEqualTo(1);
+
+        AdminLockRequestEvent publishedEvent = events.stream(AdminLockRequestEvent.class)
+                .findFirst()
+                .orElseThrow();
+
+        assertThat(publishedEvent.missionId()).isEqualTo(mission.getId());
+        assertThat(publishedEvent.robotMacAddress()).isEqualTo(robot.getMacAddress());
 
     }
 
