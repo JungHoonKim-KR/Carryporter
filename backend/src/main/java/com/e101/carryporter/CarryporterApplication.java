95bf3d3f211b64042b5c45c4844f77e9edf4b606
김정훈 <kik1232198@gmail.com>
2026-01-30 22:46:13 +0900

    [Infra] nginx 구조 변경

--- backend/src/main/java/com/e101/carryporter/CarryporterApplication.java
+++ backend/src/main/java/com/e101/carryporter/CarryporterApplication.java
@@ -10,4 +10,5 @@ public class CarryporterApplication {
         SpringApplication.run(CarryporterApplication.class, args);
     }
 
+
 }
--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -71,3 +71,4 @@ export const useAuthStore = create<AuthState>((set, get) => ({
     return get().refreshToken;
   },
 }));
+
