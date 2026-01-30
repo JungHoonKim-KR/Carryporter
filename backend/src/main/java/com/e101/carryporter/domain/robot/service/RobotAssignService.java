5b41aee80297dedde366c2e44172839b67a3597d
전준완 <joownan1024@naver.com>
2026-01-30 12:16:55 +0900

    feat: 로봇 배정 서비스 구현

---
+++ backend/src/main/java/com/e101/carryporter/domain/robot/service/RobotAssignService.java
@@ -0,0 +1,42 @@
+package com.e101.carryporter.domain.robot.service;
+
+import com.e101.carryporter.domain.mission.service.MissionService;
+import com.e101.carryporter.domain.robot.exception.RobotErrorCode;
+import com.e101.carryporter.domain.robot.repository.RobotAvailableQueueRepository;
+import com.e101.carryporter.global.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RobotAssignService {
+
+    private final RobotAvailableQueueRepository queueRepository;
+    private final MissionService missionService;
+
+    public Long assignRobotToMission(Long missionId) {
+        Long availableRobotId = null;
+
+        try {
+            // blocking
+            availableRobotId = queueRepository.acquireRobotId()
+                    .orElseThrow(() -> new BusinessException(RobotErrorCode.ROBOT_NOT_AVAILABLE));
+
+            // db 에 mission, robot 상태 변경
+            missionService.assignRobot(missionId, availableRobotId);
+
+            return availableRobotId;
+        } catch (Exception e) {
+            log.error("배차 중 error 발생!! missionId = {}", missionId, e);
+            if (availableRobotId != null) {
+                log.error("할당된 로봇 반납 robotId = {}", availableRobotId);
+                queueRepository.returnRobotToQueue(availableRobotId);
+            }
+
+            throw e;
+        }
+
+    }
+}
