59cc4f6ca75f8e1573b9a51834e831480885e3b4
강희정 <he5298@naver.com>
2026-02-03 17:27:03 +0900

    요청 경로 수정

--- admin-frontend/vite.config.ts
+++ admin-frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: 'http://localhost:8080', // 백엔드 주소
-                target: 'https://i14e101.p.ssafy.io/', // 백엔드 주소
+                target: 'https://i14e101.p.ssafy.io', // 백엔드 주소
 
         changeOrigin: true,
         secure: false,
